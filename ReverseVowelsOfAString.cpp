5bc1f9df79324b51d2aa47156b770ae1c7f07418
LeoCuper <lmpxjy@sina.com>
2017-07-10 15:09:46 +0800

    Create ReverseVowelsOfAString.cpp

---
+++ ReverseVowelsOfAString.cpp
@@ -0,0 +1,23 @@
+string reverseVowels(string s) {
+	int i, j;
+	i = 0;
+	j = s.size() - 1;
+	while (i < j)
+	{
+		if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'  )&&(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'  ))
+		{
+			char t = s[i];
+			s[i] = s[j];
+			s[j] = t;
+            i++;
+            j--;
+		}
+		else 
+		{
+			while (!(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'  )) i++;
+			while (!(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'  )) j--;
+		}
+	}
+        
+     return s;   
+    }
